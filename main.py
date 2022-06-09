d198015b9812b29ece72ebf8c297136afcb702c7
chadi@nsis.com.au <chadi@nsis.com.au>
2022-06-09 14:21:43 +1000

    research

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import pywin32
+import pyhook
+
+logger_info = "https://www.geeksforgeeks.org/design-a-keylogger-in-python/"
+service_info = "https://stackoverflow.com/questions/32404/how-do-you-run-a-python-script-as-a-service-in-windows"
+
 def main():
-  
+
